d642f23e84e0d4615294bae1f53091009dc5b8a5
SimonMcCarthy <smccarthy@netfeasa.com>
2024-10-05 21:57:35 +0100

    built bootloader and hello world for u575zi_q with overlays

--- samples/zephyr/hello-world/src/main.c
+++ samples/zephyr/hello-world/src/main.c
@@ -7,7 +7,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
-void main(void)
+int main(void)
 {
 	printk("Hello World from %s on %s!\n",
 	       MCUBOOT_HELLO_WORLD_FROM, CONFIG_BOARD);
